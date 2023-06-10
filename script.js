2a7832f1921c435ac5a28a872febac0d64bec2d2
Erick Martins <erickmartins153@hotmail.com>
2023-06-10 10:35:42 -0300

    wrap all the buttons and implement detection by bubbling and capturing the click

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const btnGrid = document.querySelector('#number-grid');
+const numberGrid = document.querySelector('#number-grid');
 let ReferenceValue = 9;
 
 for (let i = 0; i < 3; i++) {
     if (ReferenceValue >= 3) {
         for (let j = ReferenceValue - 2; j <= ReferenceValue; j++ ) {
             const btn = document.createElement('button');
-            btnGrid.appendChild(btn);
+            numberGrid.appendChild(btn);
             btn.textContent = j;
             btn.setAttribute('id', j);
         }
         ReferenceValue -= 3;
         if (ReferenceValue < 3) {
             const zero = document.createElement('button');
-            btnGrid.appendChild(zero);
+            numberGrid.appendChild(zero);
             zero.textContent = ReferenceValue;
             zero.setAttribute('id', '0');
             zero.classList.add('big-buttons');
         }
     }
 }
+
+const buttonGrid = document.querySelector('.button-grid');
+
+buttonGrid.addEventListener('click', event => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton){
+        return;
+    }
+    console.dir(event.target.id);
+})
